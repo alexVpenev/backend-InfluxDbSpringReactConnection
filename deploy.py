0f0305b6b81d67e6bb20e72b63997f644a1ded57
alexVpenev <penev027@gmail.com>
2022-12-12 01:07:37 +0100

    sql connection test

--- deploy.py
+++ deploy.py
@@ -2,6 +2,12 @@ import os
 import subprocess
 
 
+
+
+os.system('sudo docker rm -f $(sudo docker ps | grep "hypercare" | cut -d " " -f 1)')
+
+os.system('sudo docker image rm hypercare')
+
 os.system('git pull')
 
 os.system('gradle build')
@@ -9,10 +15,6 @@ os.system('gradle build')
 os.system('sudo docker build --build-arg JAR_FILE=build/libs/\*.jar -t hypercare .')
 
 os.system('sudo docker run -p 2525:8081 hypercare')
-
-os.system('sudo docker rm -f $(sudo docker ps | grep "hypercare" | cut -d " " -f 1)')
-
-os.system('sudo docker image rm hypercare')
 
 
 
